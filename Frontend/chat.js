c64c7f5e574402c10c13b2f1c2654e6a2b581990
Vivek Singh <rogerthatvivek@gmail.com>
2022-10-31 00:52:19 +0530

    Logout Function Added

--- Frontend/chat.js
+++ Frontend/chat.js
@@ -1,6 +1,32 @@
+var state;
+let logoutBtn=document.getElementById('logout')
+
+// Event Listeners
+logoutBtn.addEventListener('click', logout)
+
+//Check if already Logged In
+function checkAuthState(){
+    state=JSON.parse(sessionStorage.getItem('auth'))
+    if (state==null||state==undefined||state==''){
+        location.replace('./auth.html')
+    }else if(state.token){
+        return
+    }else{
+        location.replace('./auth.html')
+    }
+}
+
+checkAuthState()
+
+function logout(){
+    sessionStorage.removeItem('auth')
+    checkAuthState()
+}
+
 window.addEventListener('DOMContentLoaded', scrollDown)
 
 function scrollDown(){
     let chats=document.querySelector('.chats')
     chats.scrollTop=chats.scrollHeight
 }
+
