d53054f77f8569a6dbd4f653017396a84f553cb9
Vivek Singh <rogerthatvivek@gmail.com>
2022-11-02 16:43:10 +0530

    Ready for AWS Deployment

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -1,16 +1,22 @@
 require('dotenv').config();
+const pg = require('pg');
 const Sequelize=require('sequelize')
 
 const sequelize=new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     dialect: process.env.DB_DIALECT,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
+    dialectModule: pg,
     // dialectOptions: {
     //     ssl: {
     //       require: true,
     //       rejectUnauthorized: false
     //     }
-    // }
+    // },
+    dialectOptions: {
+        ssl:'Amazon RDS'
+    },
+    language: 'en'
 })
 
 module.exports=sequelize
