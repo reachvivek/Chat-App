6f668cf336b4bcdced3a3ba0e6fe4e68294c6ca5
unknown <rogerthatvivek@gmail.com>
2024-03-21 02:53:13 +0530

    Testing

--- Backend/app.js
+++ Backend/app.js
@@ -1,40 +1,46 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors=require('cors')
-const helmet = require('helmet')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const app = express();
+app.use(cors());
 
-const userRoutes=require('./routes/user')
-const groupsRoutes=require('./routes/groups')
-const chatRoutes=require('./routes/chats')
+app.get("/", (req, res) => {
+  res.send("Welcome to Chat App Backend Server");
+});
+const userRoutes = require("./routes/user");
+const groupsRoutes = require("./routes/groups");
+const chatRoutes = require("./routes/chats");
 
-const sequelize=require('./util/database')
-const Users=require('./models/users')
-const Chats=require('./models/chats')
-const Groups = require('./models/groups')
+const sequelize = require("./util/database");
+const Users = require("./models/users");
+const Chats = require("./models/chats");
+const Groups = require("./models/groups");
 
-const app=express();
+app.use(helmet());
 
-app.use(cors())
-app.use(helmet())
+app.use(bodyParser.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json({extended:false}))
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(userRoutes);
+app.use(groupsRoutes);
+app.use(chatRoutes);
 
-app.use(userRoutes)
-app.use(groupsRoutes)
-app.use(chatRoutes)
+Users.hasMany(Groups);
+Groups.belongsTo(Users);
 
-Users.hasMany(Groups)
-Groups.belongsTo(Users)
+Groups.hasMany(Chats);
+Chats.belongsTo(Groups);
 
-Groups.hasMany(Chats)
-Chats.belongsTo(Groups)
+Users.hasMany(Chats);
+Chats.belongsTo(Users);
 
-Users.hasMany(Chats)
-Chats.belongsTo(Users)
-
-sequelize.sync().then(response=>{
-    console.log(response)
-    app.listen(process.env.PORT || 3000, ()=>console.log("Server started running on Port: 3000"))
-}).catch(err=>console.log(err))
-
+sequelize
+  .sync()
+  .then((response) => {
+    console.log(response);
+    app.listen(process.env.PORT || 3000, () =>
+      console.log("Server started running on Port: 3000")
+    );
+  })
+  .catch((err) => console.log(err));
